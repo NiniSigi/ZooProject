83624e85fd294cffe8ed6396010128865513e3df
Nino Siegrist <nino.siegrist@netcetera.com>
2024-01-19 22:23:18 +0100

    Project setup

---
+++ zoobackend/src/main/java/com/example/zoobackend/ZoobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoobackendApplication.class, args);
+    }
+
+}
