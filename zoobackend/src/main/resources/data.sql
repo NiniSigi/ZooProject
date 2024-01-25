cebb79b8bd57f9757b6a5bfc8413d2b7273c559a
Ghrf6 <111276076+Ghrf6@users.noreply.github.com>
2024-01-25 17:56:45 +0100

    Update data.sql

--- zoobackend/src/main/resources/data.sql
+++ zoobackend/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO calendar (eventName, eventDate, targetGroup, eventTime)
-VALUES ('Affenschau', '2024-07-12 13:15:00', 'Familien', '13:15 - 14:15'),
+VALUES ('Affenschau', '2024-07-12 13:15:00', 'Senioren', '13:15 - 14:15'),
        ('Eulen-Exkursion', '2024-08-05 18:00:00', 'Besucher', '18:00 - 19:00'),
        ('Haustier-Tag', '2024-09-22 11:00:00', 'Kinder', '11:00 - 13:00'),
-       ('Delfin-Show', '2024-10-15 15:30:00', 'Besucher', '15:30 - 16:30'),
+       ('Delfin-Show', '2024-10-15 15:30:00', 'Senioren', '15:30 - 16:30'),
        ('Schlangen-Fütterung', '2024-11-08 14:45:00', 'Besucher', '14:45 - 15:45'),
        ('Koala-Kuschelstunde', '2024-12-03 12:30:00', 'Kinder', '12:30 - 13:30'),
        ('Safari-Nacht', '2025-01-18 20:00:00', 'Erwachsene', '20:00 - 22:00'),
